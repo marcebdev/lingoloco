2c65e60af651436745f0d64cbad5bd4641a80ff3
marcebdev <20135668+marcebdev@users.noreply.github.com>
2024-08-25 13:32:42 -0500

    add flag icons

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -4,10 +4,15 @@ import Flag from 'react-flagpack'
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      <div className="flex max-w-screen-lg mx-auto items-center justify-evenly h-full">
+      <div className="flex max-w-screen-lg mx-auto items-center gap-x-4 h-full">
         <Button variant="ghost">
-          <Flag code="NZ" />
+          <Flag code="US" size="s" className="mr-2" />
           English
+        </Button>
+
+        <Button variant="ghost">
+          <Flag code="ES" size="s" className="mr-2" />
+          Spanish
         </Button>
       </div>
     </footer>
